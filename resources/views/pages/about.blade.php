76954e0ff83bd84dc1a91194c4ca3e5c216b3f6d
Lasse Fisker <lassefiskerj@gmail.com>
2017-10-20 23:01:52 +0200

    added a questions database migrationfile

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -6,6 +6,6 @@
 <body>
 <h1>About</h1>
 <p>This is the app i'm building in the <a href="#">self taught coders course</a></p>
-<p>This app will allow people til both submit and answer programming related questions.</p>
+<p>This app will allow people til both submit and answer programming related questions.[</p>
 </body>
 </html>
